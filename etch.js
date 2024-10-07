5473701fd8dac35e6471284bb9e7485934d7043f
Atir Ali <atirvertigo@gmail.com>
2024-10-07 15:50:58 +0530

    Add hover eventlistener with randomized color effect

--- etch.js
+++ etch.js
@@ -8,7 +8,6 @@ doc.style.width = "100vw";
 doc.style.padding = "0px";
 
 const mainButtonContainer = document.querySelector(".mainButtonContainer");
-// mainButtonContainer.style.backgroundColor = "#e6e6fa";
 mainButtonContainer.style.flexGrow = "0";
 mainButtonContainer.style.display = "flex";
 mainButtonContainer.style.justifyContent = "center";
@@ -28,7 +27,6 @@ mainContainer.style.boxSizing = "border-box";
 mainContainer.style.height = "30vw";
 mainContainer.style.width = "30vw";
 mainContainer.style. margin = "2em";
-// mainContainer.style.borderColor = "10px solid #000000";
 mainContainer.style.border = "2px solid black";
 
 
@@ -43,6 +41,13 @@ function getRandomColorNumber(){
     }
 }
 
+function randomizeBgColor(){
+    let color1 = (Math.floor(Math.random()*(255-0+1)));
+    let color2 = (Math.floor(Math.random()*(255-0+1)));
+    let color3 = (Math.floor(Math.random()*(255-0+1)));
+    event.target.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+}
+
 
 
 
@@ -52,7 +57,6 @@ let square = [];
 function createGrid(){
     
     if (square.length > 0){
-        // alert(square.length);
         mainContainer.innerHTML = "";
     }
     
@@ -62,9 +66,6 @@ function createGrid(){
     // mainContainer.style.gap = `${1/(gridNum+1)}%`;
     mainContainer.style.gap = "0%";
     
-
-
-
 
     let k=0;
     for (let i = 0; i < gridNum; i++) {
@@ -80,14 +81,11 @@ function createGrid(){
             square[k].style.width = `${d}%`;
             square[k].style.flexWrap = "nowrap";
             square[k].style.boxSizing = "border-box";
-
-            // Setting random background color of the divs .
-            color1 = (Math.floor(Math.random()*(255-0+1)));
-            color2 = (Math.floor(Math.random()*(255-0+1)));
-            color3 = (Math.floor(Math.random()*(255-0+1)));
-            square[k].style.backgroundColor = `rgb(${color1},${color2},${color3})`;
-            square[k].style.color = "black";  
+            square[k].style.border = ".1px solid black";
             
+
+            // Setting eventlistener to randomize background color of the divs .
+            square[k].addEventListener("mouseover", () => randomizeBgColor());            
             mainContainer.appendChild(square[k]);
             
             k++;
@@ -109,126 +107,4 @@ selectionBtn.addEventListener("click", () => createGrid());
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const doc = document.querySelector("body");
-// doc.style.display = "flex";
-// doc.style.flexDirection = "column";
-// doc.style.height = "100vh";
-
-// const mainButtonContainer = document.querySelector(".mainButtonContainer");
-// mainButtonContainer.style.backgroundColor = "#e6e6fa";
-// mainButtonContainer.style.flexGrow = "0";
-// mainButtonContainer.style.display = "flex";
-// mainButtonContainer.style.justifyContent = "center";
-// mainButtonContainer.style.height = "5vh";
-// mainButtonContainer.style.flexShrink = "0";
-
-// const selectionBtn = document.querySelector(".mainButton");
-
-// const mainContainer = document.querySelector(".mainContainer");
-// mainContainer.style.backgroundColor = "black";
-// mainContainer.style.display = "flex";
-// mainContainer.style.justifyContent = "space-around";
-// mainContainer.style.alignItems = "center";
-// // mainContainer.style.gap = `${1/26}vw`;
-// mainContainer.style.flexWrap = "wrap";
-
-
-
-// mainButtonContainer.appendChild(selectionBtn);
-
-// let gridNum = 1;
-// let square = [];
-
-// function createGrid(){
-    
-//     if (square.length > 0){
-//         let throwAwayVar;
-//         alert(square.length);
-//         for (let div = 1; div < (square.length + 1); div++){
-//             alert(square[div]);
-//             throwAwayVar = square;
-//             throwAwayVar.removeChild(throwAwayVar.children[div]);
-//             // square[div].remove()
-//             alert(square.length)
-//         }
-//         // mainContainer.removeChild(child);
-//         alert("Kicker")
-        
-//     }
-    
-//     square = [];
-
-//     alert(square.length);
-
-//     gridNum = parseInt(document.getElementById("myInput").value,10);
-//     mainContainer.style.gap = `${1/(gridNum+1)}vw`;
-    
-//     let k=0;
-//     for (let i = 0; i < gridNum; i++) {
-//         for (let j = 0; j < gridNum; j++) {
-//             square[k+1] = document.createElement("div");
-//             square[k+1].style.display = "flex"
-//             square[k+1].style.justifyContent = "space-around";
-//             square[k+1].style.alignItems = "center";
-//             square[k+1].textContent = k+1;
-//             square[k+1].style.color = "#ffffff";
-//             let d = (100/(gridNum+1))
-//             square[k+1].style.height = `${d}vw`;
-//             square[k+1].style.width = `${d}vw`;
-//             square[k+1].style.borderColor = "Black";
-//             square[k+1].style.flexWrap = "wrap";
-//             if ((k+1)%3==0){
-//                 square[k+1].style.backgroundColor = "#6a5acd";
-//             }
-//             else if ((k+1)%3==1){square[k+1].style.backgroundColor = "#008080";}
-//             else {square[k+1].style.backgroundColor = "#4682b4";}
-
-
-//             mainContainer.appendChild(square[k+1]);
-            
-//             k++;
-//         }        
-//     }
-// }
-
-
-
-
-// selectionBtn.addEventListener("click", () => createGrid());
 
